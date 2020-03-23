d9c372bc9da1074b14dc1a56c2ecf9d91a10f5f2
Alok Singh <aloksingh8979@gmail.com>
2020-03-23 14:37:05 +0530

    ViewModel class added
    View Model added

---
+++ Vidly/VidlyClassLibrary/ViewModel/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace VidlyClassLibrary.ViewModel
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public int RollNumber { get; set; }
+        public IEnumerable<Course> Course { get; set; }
+    }
+}
