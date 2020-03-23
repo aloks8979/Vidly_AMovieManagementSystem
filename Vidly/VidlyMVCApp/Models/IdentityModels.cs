ac3a2771ca11cf51aeea217579139492f9e8a274
Alok Singh <aloksingh8979@gmail.com>
2020-03-24 02:22:14 +0530

    DB migration added
    DB migration added

--- Vidly/VidlyMVCApp/Models/IdentityModels.cs
+++ Vidly/VidlyMVCApp/Models/IdentityModels.cs
@@ -20,9 +20,12 @@ namespace VidlyMVCApp.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            System.AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
         }
 
         public static ApplicationDbContext Create()
