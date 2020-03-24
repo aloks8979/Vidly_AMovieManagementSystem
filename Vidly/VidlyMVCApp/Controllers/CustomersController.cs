c9f558295485678f51350d5c9adfa4e2d3a2285a
Alok Singh <aloksingh8979@gmail.com>
2020-03-24 17:41:58 +0530

    Added membershipt type and db model
    Added membershipt type and db model

--- Vidly/VidlyMVCApp/Controllers/CustomersController.cs
+++ Vidly/VidlyMVCApp/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,33 +11,63 @@ namespace VidlyMVCApp.Controllers
 {
     public class CustomersController : Controller
     {
+        //ApplicationDbContext _context;
+        //public CustomersController()
+        //{
+        //    //_context = new ApplicationDbContext();
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    //_context.Dispose();
+        //}
+
         // GET: Customers
         public ActionResult Index()
         {
-            var movies = new Movie()
-            {
-                Name = "Bajirao Mastani"
-            };
-
-            var customers = new List<Customer>()
-            {
-                new Customer{ Name = "Alok Singh" },
-                new Customer{ Name = "Anjali Singh" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movies,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            var customers = GetViewModel();
+            // _context.Customers.Include(c => c.MembershipType).ToList();
+            
+            return View(customers);
         }
 
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
 
-            return View("CustomerDetail");
+            if(customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View("CustomerDetail", customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer{ Id = 1, Name = "Alok Singh"},
+                new Customer{ Id = 2, Name = "Anjali Singh"}
+            };
+        }
+
+        private RandomMovieViewModel GetViewModel()
+        {
+            var customer = new List<Customer>()
+            {
+                new Customer{ Id = 1, Name = "Alok Singh"},
+                new Customer{ Id = 2, Name = "Anjali Singh"}
+            };
+
+            var movie = new Movie() { Name = "Bajirao Mastani"};
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Customers = customer,
+                Movie = movie
+            };
+
+            return viewModel;
         }
     }
 }
